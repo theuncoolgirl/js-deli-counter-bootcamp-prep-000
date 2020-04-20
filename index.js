a4bc1cdf261f51f9a81fd55ee7754ea1a08b3c88
Erin Shields <elshields92@gmail.com>
2020-04-20 13:11:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length = 0) {
+    console.log("There is nobody waiting to be served!")
+  }
+  else {
+    console.log(${katzDeliLine[0]})
+  }
+}
