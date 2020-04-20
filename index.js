9fb3a0fe061fa1dc8516ee1eeb2b3ae2a00e18d4
Erin Shields <elshields92@gmail.com>
2020-04-20 13:48:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,11 +13,15 @@ function nowServing(katzDeliLine) {
   }
 }
 
-function currentLine(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
+function currentLine(line) {
+  if (line.length === 0) {
     return `The line is currently empty.`;
   } else {
-    for (let i = 0; katzDeliLine.length > i; i++) {
+    var array = []
+    var place = i + 1
+    var name = line[i]
+    for (let i = 0; line.length > i; i++) {
+      array.push("${place}. ${name}")
       return `The line is currently: ${katzDeliLine}.`
     }
   }
