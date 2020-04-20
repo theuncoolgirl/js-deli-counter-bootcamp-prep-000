7a0d7a9c645048900052edc13a67384fb2f4667b
Erin Shields <elshields92@gmail.com>
2020-04-20 13:42:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  if (line.length === 0) {
+  if (katzDeliLine.length === 0) {
     return `The line is currently empty.`;
   } else {
     for (let i = 0; katzDeliLine.length > i; i++) {
