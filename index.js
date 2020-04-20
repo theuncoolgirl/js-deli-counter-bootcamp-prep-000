3e7c74206bcfe8c170ca3b143e22a0bb32606ff8
Erin Shields <elshields92@gmail.com>
2020-04-20 13:13:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
+  if (katzDeliLine[0] === undefined) {
     console.log("There is nobody waiting to be served!")
   }
   else {
